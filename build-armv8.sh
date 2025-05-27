f27c8870168b387202e9ae9da0b432625a30889a
RetroMike <michaeldlesk@gmail.com>
2025-05-27 15:13:13 -0500

    Update build-armv8.sh

--- build-armv8.sh
+++ build-armv8.sh
@@ -7,9 +7,9 @@ make distclean || echo clean
 rm -f config.status
 ./autogen.sh || echo done
 
-CFLAGS="-O2 -march=armv8-a+crypto+sha2+aes -Wall -flax-vector-conversions" ./configure  --with-curl  --host=aarch64-cortexa76-elf --build=x86_64-pc-linux-gnu --target=aarch64-cortexa76-elf
-#CFLAGS="-O2 -march=armv8-a+crypto+sha2+aes -Wall -flax-vector-conversions" ./configure  --with-curl
+#CFLAGS="-O2 -march=armv8-a+crypto+sha2+aes -Wall -flax-vector-conversions" ./configure  --with-curl  --host=aarch64-cortexa76-elf --build=x86_64-pc-linux-gnu --target=aarch64-cortexa76-elf
+CFLAGS="-O2 -march=armv8-a+crypto+sha2+aes -Wall -flax-vector-conversions" ./configure  --with-curl
 
 make -j $(nproc)
 
-strip -s cpuminer
+#strip -s cpuminer
