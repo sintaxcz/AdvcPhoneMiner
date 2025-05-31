9794e6b2368b00c5eaea6d6684432dc589ac9dde
RetroMike <michaeldlesk@gmail.com>
2025-05-31 14:45:27 -0500

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -8,7 +8,7 @@ cd advc_miner
 ./build-armv8.sh
 cd ~
 #echo "screen -dmS miner ~/ccminer/ccminer -a verus -o stratum+tcp://na.luckpool.net:3960 -u RKbgnfWAbL43K1HrxLoWcDzRmTspYFRj6U -p x -t 6" > startup.sh
-echo "screen -dmS miner ~/advcphoneminer/cpuminer -a YespowerAdvc -o $1 -u $2 -p $3 -t $4" > startup.sh
+echo "screen -dmS miner ~/advc_miner/cpuminer -a YespowerAdvc -o $1 -u $2 -p $3 -t $4" > startup.sh
 chmod +x startup.sh
 mkdir -p ~/.termux/boot
 wget https://raw.githubusercontent.com/TheRetroMike/VerusCliMining/refs/heads/main/termux_boot_startup.sh -O ~/.termux/boot/termux_boot_startup.sh
